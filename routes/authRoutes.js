c99f761f318a989b20116b3a7d250769416b9c40
adeel <adeel153.mughal@gmail.com>
2026-05-31 17:20:00 +0500

    Create Auth API

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const {registerController, loginController} = require("../controllers/authController");
+const {registerController, loginController, forgotPassword} = require("../controllers/authController");
 
 const router = express.Router();
 router.post('/register', registerController)
 router.post('/login', loginController)
+router.post('/forgot-password', registerController)
+router.post('/change-password', forgotPassword);
 module.exports = router;
